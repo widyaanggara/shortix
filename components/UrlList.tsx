0655d7394e01f60bc2300ede0859b4f88fd75db9
Widya Anggara <anggarawidya005@gmail.com>
2025-03-21 13:17:59 +0800

    first commit

--- components/UrlList.tsx
+++ components/UrlList.tsx
@@ -16,9 +16,22 @@ const UrlList = () => {
     const [urls, setUrls] = useState<Url[]>([]);
     const [copied, setCopied] = useState<boolean>(false);
     const [copyUrl, setCopyUrl] = useState<string>('');
+    const [isLoading, setIsLoading] = useState<boolean>(false);
     
     const shortenerUrl = (code: string) => `${process.env.NEXT_PUBLIC_BASE_URL}/${code}`;
 
+    const fetchUrls = async () => {
+        setIsLoading(true);
+        try {
+            const response = await fetch('/api/urls');
+            const data = await response.json();
+            setUrls(data);
+        } catch (error) {
+            console.error('Error fetching URLs', error);
+        } finally {
+            setIsLoading(false);
+        }
+    };
 
     const handleCopyUrl = (code: string) => {
         const fullUrl = shortenerUrl(code);
@@ -35,6 +48,31 @@ const UrlList = () => {
     useEffect(() => {
         fetchUrls();
     }, []);
+
+    if(isLoading) {
+        return (
+            <div className='animate-pulse'>
+                <div className='h-8 bg-gray-200 rounded w-1/4 mb-4'></div>
+                <ul className='space-y-2'>
+                    {[1, 2, 3].map((num) => (
+                        <li
+                            key={num}
+                            className='flex items-center gap-2 rounded-md border bg-card p-4 text-card-foreground justify-between'
+                        >
+                            <div className='h-4 bg-gray-200 rounded w-1/2'></div>
+                            <div className='flex items-center gap-3'>
+                                <div className='h-5 w-5 bg-gray-200 rounded'></div>
+                                <span className='flex items-center gap-2'>
+                                    <div className='h-4 w-4 bg-gray-200 rounded'></div>
+                                    <div className='h-4 w-10 bg-gray-200 rounded'></div>
+                                </span>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
+    }
 
     return (
         <div className="relative">
